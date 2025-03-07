289234073814586a461d445a1e9de7aed8097437
pVaprog <pva082005@gmail.com>
2025-03-07 16:45:30 +0300

    Chached Task1 and Task2

--- t1/main.c
+++ t1/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int a = 10, b = 20;
+    int a, b;
+
+	// Ввод значений пользователя
+	printf("Enter a: ");	
+	scanf("%d", &a);
+	printf("Enter b: ");	
+	scanf("%d", &b);
+	
+	// Указатели на переменные
     int *ptrA = &a;
     int *ptrB = &b;
 
     *ptrA *= 2;  // Увеличиваем значение a в 2 раза
     *ptrB /= 2;  // Уменьшаем значение b в 2 раза
 
+	printf("Changed numbers:");
     printf("a = %d, b = %d\n", a, b);
 
     return 0;
--- t2/main.c
+++ t2/main.c
@@ -6,9 +6,12 @@ int main() {
     float *ptr2 = (float *)malloc(sizeof(float));
     float *ptr3 = (float *)malloc(sizeof(float));
 
-    *ptr1 = 10.5;
-    *ptr2 = 20.5;
-    *ptr3 = 30.5;
+	printf("Enter ptr1: ");
+	scanf("%f", ptr1);
+	printf("Enter ptr1: ");
+	scanf("%f", ptr2);
+	printf("Enter ptr1: ");
+	scanf("%f", ptr3);
 
     printf("Before swap: ptr1 = %.2f, ptr2 = %.2f\n", *ptr1, *ptr2);
 
